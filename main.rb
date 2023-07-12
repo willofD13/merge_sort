9953f354eb1d9c86a802af24f2134134b11fb097
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-07-12 20:48:33 +0300

    Recursively divide the arrays

--- main.rb
+++ main.rb
@@ -1,5 +1,15 @@
 def merge_sort(array)
+  n = array.length
+  a = []
+  b = []
   if array.length == 1
     return 
   else 
-    
+    a = array.slice(0,n/2)
+    b = array.slice(n/2,n)
+    merge_sort(a)
+    merge_sort(b)
+  end 
+end
+
+merge_sort([1,2,5,3,6,2,6,8])
