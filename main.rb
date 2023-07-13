9226b85367ffad7cd6168d34678bc9e235ae3819
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-07-13 13:49:33 +0300

    Store return values of recursion in variables

--- main.rb
+++ main.rb
@@ -5,33 +5,34 @@ def merge_sort(array)
   right_half = []
   
   if n == 1
-    return
+    return array
   else 
-    left_half = array.slice(0,n/2)
-    right_half = array.slice(n/2,n)
-    merge_sort(left_half)
-    merge_sort(right_half)
+    left_half = merge_sort(array.slice(0,n/2))
+    right_half = merge_sort(array.slice(n/2,n))
     a = 0 
     b = 0
     c = 0
-    array.clear #the original array empties and becomes the new merged array
+    sorted = []
+   
     while a < left_half.length && b < right_half.length
       if left_half[a] < right_half[b]
-      array[c] = left_half[a]
+      sorted[c] = left_half[a]
       a += 1
       else 
-        array[c] = right_half[b]
+        sorted[c] = right_half[b]
         b += 1
       end
       c += 1 
     end
     if a == left_half.length
-      array.push(right_half[b..right_half.length]).flatten!
+      sorted.push(right_half[b..right_half.length]).flatten!
     else 
-      array.push(left_half[a..left_half.length]).flatten!
+      sorted.push(left_half[a..left_half.length]).flatten!
     end
     #binding.pry
-  end 
+    sorted
+  end
+  sorted 
 end
 
 p merge_sort([1,5,3,6,7,4,8])
